662f1dc9d590c134ffea46da79e2b3ae9cd600c2
Bettina Meier <bmeier01@web.de>
2019-10-25 00:04:40 -0400

    added number of reviews

--- singlemalts/spiders/singlemalts_spider.py
+++ singlemalts/spiders/singlemalts_spider.py
@@ -10,46 +10,73 @@ class SingleMaltsSpider(Spider):
     # get the 500 most popular ones
     start_urls = ['https://www.masterofmalt.com/country-style/scotch/single-malt-whisky/']
     #start_urls = ['https://www.masterofmalt.com/country-style/scotch/single-malt-whisky/name/']
-    # # request to start_urls what do we do parse
 
     def parse(self, response):
         # get numbers of whiskies per page class="col-md-12"
         id_info = response.xpath('//*[@id="productBoxWideContainer"]//div[@class = "boxBgr product-box-wide h-gutter js-product-box-wide"]/@id').extract()
         num_per_page = len(id_info)
-        total_pages = math.ceil(500/num_per_page) # hardcoded number, get the 500 most popular scotch whiskies!
-    # find result_urls
-        result_urls = ["https://www.masterofmalt.com/country-style/scotch/single-malt-whisky/name/{}/".format(x) for x in range(1, total_pages+1)]
+        total_pages = math.ceil(800/num_per_page) # hardcoded number, get the 600 most popular scotch whiskies!
 
-#       if
+        # find result_urls
+        result_urls = ["https://www.masterofmalt.com/country-style/scotch/single-malt-whisky/{}/".format(x) for x in range(1, total_pages+1)]
 
-        for url in result_urls[:1]: # limit right now to first 2
-            yield Request(url= url, callback = self.parse_result_page)
+        # identify all product pages but only scrape those that have reviews
+#        specifier = []
+#        for i in range(0,len(id_info)):
+#            specifier.append(re.findall(r'\d+', id_info[i])[1])
+#        rating_paths = []
+#        for i in range(0,len(specifier)):
+#            rating_paths.append(response.xpath('//*[@id="ContentPlaceHolder1_ctl{}_containerRating"]'.format(specifier[i])).extract())
+#
+#        products_with_rating = [elem !=[] for elem in rating_paths]
+#
+#        all_product_urls = response.xpath('//*[@id="productBoxWideContainer"]//div[@class = "boxBgr product-box-wide h-gutter js-product-box-wide"]/@data-product-url').extract()
+#        print(len(all_product_urls))
+#        print('=' * 50)
+
+        # take product_urls only of those for which rating_paths is not empty
+        #def filter_by_rating (lis1, lis2):
+#        product_urls = []
+#        for idx in range(0,len(products_with_rating)):
+#            if products_with_rating[idx]:
+#                product_urls.append(all_product_urls[idx])
+
         # this is a scrapy request, which method to use to parse
+#         product_urls = all_product_urls[list(map(lambda item: item!=[], rating_paths))]
 
-    def parse_result_page(self, response):
-        product_urls = response.xpath('//*[@id="productBoxWideContainer"]//div[@class = "boxBgr product-box-wide h-gutter js-product-box-wide"]/@data-product-url').extract()
-        print(len(product_urls))
-        print('=' * 50)
+        #product_urls = filter_by_rating(products_with_rating, all_product_urls)
 
-#        price_paths = response.xpath('//*[@id="ContentPlaceHolder1_ctl00_pricesWrapper"]')
+#        products_with_rating = [elem !=[] for elem in rating_paths]
+#        product_urls = list({key:value for (key,value) in dict(zip(products_with_rating, all_product_urls)).items() if value == True}.keys())
+#
+#        product_urls = []
+#        if item in rating_paths !=[]:
+#            product_urls.append(all_product_urls[index(item)])
+
 
 #       try:
-#           price = resp
+#           price_paths = response.xpath('//*[@id="ContentPlaceHolder1_ctl00_pricesWrapper"]//span/text()').extract()
+#            price = re.findall(r'\$\d+\.\d+', ''.join(price))
 #       except:
 #           price = ""
+        for url in result_urls[:1]: # limit right now to first 2
+            yield Request(url= url, callback = self.parse_result_page)
 
+    def parse_result_page(self, response):
+
+        product_urls = response.xpath('//*[@id="productBoxWideContainer"]//div[@class = "boxBgr product-box-wide h-gutter js-product-box-wide"]/@data-product-url').extract()
 
         for url in product_urls:
-                yield Request(url= url, callback = self.parse_product_page)
-                # obtain Whisky details (Country, Region, Distillery, Age) from box
+            yield Request(url= url, callback = self.parse_product_page)
+                
 
     def parse_product_page(self, response):
-
-#        try:
-#            number_of_reviews = response.xpath('//*[@id="ContentPlaceHolder1_productRating_productUserRating"]/a/div[2]/text()').extract()
-#           review_nr = re.findall(r'\d+', number_of_reviews)
-#        except:
-#            number_of_reviews = ''
+        # obtain Whisky details (review numbers, rating, region, distillery, age, tasting notes)
+        try:
+            number_of_reviews = response.xpath('//*[@id="ContentPlaceHolder1_productRating_productUserRating"]/a/div[2]/text()').extract()
+            number_of_reviews = re.findall(r'\d+', ''.join(number_of_reviews))
+        except:
+            number_of_reviews = ''
 
         try:
             name = response.xpath('//*[@id="ContentPlaceHolder1_pageH1"]/text()').extract()
@@ -68,6 +95,7 @@ class SingleMaltsSpider(Spider):
 
         try:
             age = response.xpath('//*[@id="ContentPlaceHolder1_ctl00_ctl00_wdYearsMatured"]/span[2]/a/text()').extract()
+            age = re.findall(r'\d+', ''.join(age))
         except:
             age = ''
 
@@ -76,8 +104,6 @@ class SingleMaltsSpider(Spider):
             tasting_notes = response.xpath('//*[@id="ContentPlaceHolder1_ctl00_ctl02_TastingNoteBox_ctl00_breakDownTastingNote"]//p/text()').extract()
         except:
             tasting_notes = ''
-
-        # price not on this page
 
         try:
             rating = response.xpath('//*[@id="ContentPlaceHolder1_productRating_productUserRating"]/a/div[1]/meta[3]/@content').extract()
@@ -92,7 +118,7 @@ class SingleMaltsSpider(Spider):
         item['age'] = age
         item['tasting_notes'] = tasting_notes
         item['rating'] = rating
-#        item['review_nr'] = review_nr
+        item['number_of_reviews'] = number_of_reviews
         #item['price'] = price
 
         yield item
