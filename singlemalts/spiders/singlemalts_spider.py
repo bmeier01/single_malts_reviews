a91fe8d4a6e7d0824ca7006798b73cd9a15ecbc9
Bettina Meier <bmeier01@web.de>
2019-10-26 21:00:35 -0400

    incl price

--- singlemalts/spiders/singlemalts_spider.py
+++ singlemalts/spiders/singlemalts_spider.py
@@ -1,63 +1,63 @@
 from scrapy import Spider, Request
 from singlemalts.items import SinglemaltsItem
 
-import re # needs to be imported here if used in shell
+# import use packages
+import re
 import math
 
 class SingleMaltsSpider(Spider):
     name = 'singlemalts_spider'
     allowed_domains = ['www.masterofmalt.com']
-    # get the 500 most popular ones
+    # get the 750 most popular ones
     start_urls = ['https://www.masterofmalt.com/country-style/scotch/single-malt-whisky/']
-    #start_urls = ['https://www.masterofmalt.com/country-style/scotch/single-malt-whisky/name/']
+
 
     def parse(self, response):
         # get numbers of whiskies per page class="col-md-12"
         id_info = response.xpath('//*[@id="productBoxWideContainer"]//div[@class = "boxBgr product-box-wide h-gutter js-product-box-wide"]/@id').extract()
         num_per_page = len(id_info)
-        total_pages = math.ceil(800/num_per_page) # hardcoded number, get the 600 most popular scotch whiskies!
+        total_pages = math.ceil(800/num_per_page) # hardcoded number. going through all may not be useful for customer, many very specialised and unique!
 
-        # find result_urls
+        # retrieve result_urls
         result_urls = ["https://www.masterofmalt.com/country-style/scotch/single-malt-whisky/{}/".format(x) for x in range(1, total_pages+1)]
 
-        for url in result_urls[:1]: # limit right now to first 2
+        for url in result_urls[:30]: # limit to 750 most popular whiskies
             yield Request(url= url, callback = self.parse_result_page)
 
     def parse_result_page(self, response):
-
+        # retrieve result_urls
         product_urls = response.xpath('//*[@id="productBoxWideContainer"]//div[@class = "boxBgr product-box-wide h-gutter js-product-box-wide"]/@data-product-url').extract()
 
-         price_paths = []
-         for i in range(0,len(product_urls)):
-                price_paths.append(response.xpath('//*[@id="ContentPlaceHolder1_ctl{}_containerRating"]'.format(i))).extract()
+        id_info = response.xpath('//*[@id="productBoxWideContainer"]//div[@class = "boxBgr product-box-wide h-gutter js-product-box-wide"]/@id').extract()
+        specifier = []
+        for i in range(0,len(id_info)):
+            specifier.append(re.findall(r'\d+', id_info[i])[1])
 
+        # get prices from first page
+        price_links = []
+        for i in specifier:
+            price_links.append('//*[@id="ContentPlaceHolder1_ctl{}_pricesWrapper"]'.format(specifier[int(i)]))
 
-#        all_product_urls = response.xpath('//*[@id="productBoxWideContainer"]//div[@class = "boxBgr product-box-wide h-gutter js-product-box-wide"]/@data-product-url').extract()
-#        print(len(all_product_urls))
-#        print('=' * 50)
+        price = []
+        for link in price_links:
+            try:
+                price_extract = response.xpath(link).extract()
+                price_extract = re.findall(r'\$\d+\.\d+', ''.join(price_extract))
+            except:
+                price_extract = ""
+            price.append(price_extract)
 
-#        product_urls = []
-#        for i in rating_paths !=[]:
-#            product_urls.append(all_product_urls[index(i)])
+        #product_urls = response.xpath('//*[@id="productBoxWideContainer"]//div[@class = "boxBgr product-box-wide h-gutter js-product-box-wide"]/@data-product-url').extract()
 
-
-        #price_xpaths = ["https://www.masterofmalt.com/country-style/scotch/single-malt-whisky/{}/".format(x) for x in range(1, total_pages+1)]
-        #try:
-        #    price_paths = response.xpath('//*[@id="ContentPlaceHolder1_ctl00_pricesWrapper"]//span/text()').extract()
-        #    price = re.findall(r'\$\d+\.\d+', ''.join(price))
-        #except:
-        #    price = ""
-
-        product_urls = response.xpath('//*[@id="productBoxWideContainer"]//div[@class = "boxBgr product-box-wide h-gutter js-product-box-wide"]/@data-product-url').extract()
-
-        for url in product_urls:
-            yield Request(url= url, callback = self.parse_product_page, meta={'price':price})
-
+        # pass price for each product with product url
+        for i in range(len(product_urls)):
+            yield Request(url= product_urls[i], meta={'price':price[i]}, callback = self.parse_product_page)
 
     def parse_product_page(self, response):
-        # obtain Whisky details (review numbers, rating, region, distillery, age, tasting notes)
+
         price = response.meta['price']
 
+        # obtain Whisky details (review numbers, rating, region, distillery, age, tasting notes)
         try:
             number_of_reviews = response.xpath('//*[@id="ContentPlaceHolder1_productRating_productUserRating"]/a/div[2]/text()').extract()
             number_of_reviews = re.findall(r'\d+', ''.join(number_of_reviews))
@@ -105,6 +105,6 @@ class SingleMaltsSpider(Spider):
         item['tasting_notes'] = tasting_notes
         item['rating'] = rating
         item['number_of_reviews'] = number_of_reviews
-        #item['price'] = price
+        item['price'] = price
 
         yield item
